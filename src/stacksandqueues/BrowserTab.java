29869f9f360da65f592be2039b32ff72e20ca12d
ghoshanirban <39007826+ghoshanirban@users.noreply.github.com>
2024-05-30 22:17:36 -0400

    Thu May 30 20:39:39 EDT 2024

--- src/stacksandqueues/BrowserTab.java
+++ src/stacksandqueues/BrowserTab.java
@@ -4,42 +4,42 @@ import java.util.*;
 
 public class BrowserTab {
     String currentPage = "";
-    private final LinkedStack<String> previousPages = new LinkedStack<>(),
-                                               nextPages = new LinkedStack<>();
+    private final LinkedStack<String> backPages = new LinkedStack<>(),
+                                      forwardPages = new LinkedStack<>();
     public static String getTimeStamp(){ return "[" + new Date() + "] "; }
 
     public BrowserTab() { System.out.println(getTimeStamp() + "New tab opened."); }
     
     public void typeAndGoNewSite(String newPage) {
         if( !currentPage.isEmpty() ) // some page is being viewed currently
-            previousPages.push(currentPage); // send the current page to the previousPages DLL
+            backPages.push(currentPage); // send the current page to the previousPages DLL
 
         currentPage = newPage;
         System.out.println(getTimeStamp() + "Currently viewing: " + currentlyViewing());
     }
 
     public void clickOnGoBackButton() {
-        if( previousPages.isEmpty() ) {
+        if( backPages.isEmpty() ) {
             System.out.println(getTimeStamp() + "Back button is greyed out (unavailable).");
             return;
         }
         else {
             System.out.print(getTimeStamp() + "Back button is clicked. ");
-            nextPages.push(currentPage);
-            currentPage = previousPages.top();
-            previousPages.pop();
+            forwardPages.push(currentPage);
+            currentPage = backPages.top();
+            backPages.pop();
         }
         System.out.println("Currently viewing: " + currentlyViewing());
     }
 
     public void clickOnGoForward() {
-        if( nextPages.isEmpty() ) {
+        if( forwardPages.isEmpty() ) {
             System.out.println(getTimeStamp() + "Forward button is greyed out (unavailable).");
             return;
         }
         else {
-            previousPages.push(currentPage);
-            currentPage = nextPages.pop();
+            backPages.push(currentPage);
+            currentPage = forwardPages.pop();
         }
         System.out.println(getTimeStamp() + "Forward button is clicked. Currently viewing: " + currentlyViewing());
     }
@@ -49,12 +49,12 @@ public class BrowserTab {
     public String toString() {
         StringBuilder prettyString = new StringBuilder();
 
-        for (String site : previousPages)
+        for (String site : backPages)
             prettyString.append(site).append(" ");
 
         prettyString.append(" ***").append(currentPage).append("*** ");
 
-        for (String site : nextPages)
+        for (String site : forwardPages)
             prettyString.append(site).append(" ");
 
         return prettyString.toString();
